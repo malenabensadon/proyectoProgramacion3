cdbc1e79cd4ef7c7f845412987f32963795ee565
justinakelly <justinakelly@udesa.edu.ar>
2022-09-05 21:54:27 -0300

    Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,7 +21,8 @@ class NavBar extends Component {
     }
     render() {
         return (
-            <div className='container'>
+            <header>
+                 {/* <div className='container'>
                 <div className='search-container'>
                     <form>
                         <input type="text" className='search-input' onChange={(event) => this.buscarResultado(event)} value={this.state.busqueda} />
@@ -34,7 +35,37 @@ class NavBar extends Component {
                     <Link to='/best-albums'>Best Albums</Link>
                     <Link to='/favourite-artists'>Favourite Artists</Link>
                 </div>
-            </div>
+            </div>  */}
+            
+            <section className="logo">
+                <div className="imagenes-logo">
+                    <img src="img/anytime-slogan.PNG" alt="Music Anytime" className="slogan"/>
+                    <img src="img/logo3.PNG" alt="Logo Planet Music"/>
+                    <img src="img/anywhere-slogan.PNG" alt="Music Anywhere" className="slogan"/>
+                </div>
+                <div className="alerta">
+                    <p className="probando1">The field can't be empty</p>
+                    <p className="probando2">You should enter at least three characters</p>
+                </div>  
+            </section>
+            <nav className="navegador">
+                <img src="img/logo3.PNG" alt="Logo Planet Music"/>
+                <form action="search-results.html" method="GET" className="search">
+                    <input type="text" name="search" value="" placeholder="Search..." onChange={(event) => this.buscarResultado(event)} value={this.state.busqueda} />
+                    <button type="submit"><i className="fa fa-search"></i></button>
+                    <i className="fas fa-exclamation-circle closeIcon"></i>
+                </form>
+                <ul>
+                    <li id="home"><Link to='/'>Home</Link></li>
+                    <li id="vermas"><Link to='/hot-tracks'>Hot Tracks</Link></li>
+                    <li id="vermas"> <Link to='/best-albums'>Best Albums</Link></li>
+                    <li id="vermas"> <Link to='/favourite-artists'>Favourite Artists</Link></li>
+
+                    <li id="my-playlist"> <Link to='/favoritos'>Favourites</Link></li>
+                    
+                </ul>
+            </nav>
+        </header>
         )
     }
 }
