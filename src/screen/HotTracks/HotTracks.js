2fa0c2251ba2b9f18be16ca6cfae2db48c97d7af
malenabensadon <malenabensadon@MacBook-Pro-de-Malena.local>
2022-09-05 21:08:15 -0300

    cambio

--- src/screen/HotTracks/HotTracks.js
+++ src/screen/HotTracks/HotTracks.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 
 class HotTracks extends Component {
+    constructor() {
+        super();
+        this.state = {
+            tracks: [],
+        }
+    }
+    componentDidMount(){
+        fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/tracks')
+			.then( response => response.json())
+			.then( data => this.setState(
+				{ tracks: data.data }
+			))
+
+    }
+
     render() {
         return (
             <div>Hot Tracks</div>
+            
+
+
+            
         )
     }
 }
