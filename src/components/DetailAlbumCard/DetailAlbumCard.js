098fa43fd55d99ab8f3faadf9bd9e8582275a626
malenabensadon <malebensadon@gmail.com>
2022-09-19 17:06:19 -0300

    detalle

--- src/components/DetailAlbumCard/DetailAlbumCard.js
+++ src/components/DetailAlbumCard/DetailAlbumCard.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import './DetailAlbumCard.css'
 
 class DetailAlbumCard extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
 
         }
